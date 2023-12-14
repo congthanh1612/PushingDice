8922951f9db5d46704470907de28a6c05bf8f200
user <user@admins-MacBook-Pro.local>
2023-12-14 13:38:49 +0700

    Update UI neon

--- assets/Scripts/level.js
+++ assets/Scripts/level.js
@@ -3,28 +3,27 @@ const Emitter = require('mEmitter');
 cc.Class({
     extends: cc.Component,
 
-    properties: {},
+    properties: {
+        levelLock: cc.SpriteFrame,
+        levelUnlock: cc.SpriteFrame,
+    },
 
     start() {
         Emitter.instance.registerEvent('COMPLETE_LEVEL', this.onCompleteLevel.bind(this));
     },
 
     selectedLevel() {
-        let isLock = this.node.children[1].active;
-        if (!isLock) {
+        let isLock = this.node.children[0].active;
+        if (isLock) {
             let level = this.node.name;
             Emitter.instance.emit('SELECTED_LEVEL', level);
         }
     },
 
     onCompleteLevel(data) {
-        if (data.level == this.node.name) {
-            this.node.children[2].active = true;
-        }
         if (data.levelUnlock == this.node.name) {
             this.node.children[0].active = true;
-            this.node.children[1].active = false;
-            this.node.color = new cc.Color(53, 150, 255);
+            this.node.getComponent(cc.Sprite)._spriteFrame = this.levelUnlock;
         }
     }
 });
