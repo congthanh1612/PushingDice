b75b0b7bddc5d0155295e39ccb34e6a46d34b46f
congthanh1612 <daocongthanh43@gmail.com>
2023-12-18 11:20:05 +0700

    Thanh_OptimizeMovementCode

--- assets/Scripts/DiceController.js
+++ assets/Scripts/DiceController.js
@@ -1,17 +1,125 @@
 var Dice = require('Dice'); 
+var Map = require('Map');
+const DICE_DIRECTION = {
+    LEFT: 1,
+    RIGHT: 2,
+    UP: 3,
+    DOWN: 4
+};
 cc.Class({
     extends: cc.Component,
 
     properties: {
         atlas: cc.SpriteAtlas,
         _indexDice:1,
+        map: cc.Node,
+        btnHolder: cc.Node,
+        moves: cc.Label,
+
+        diceFace: {
+            type: [cc.Label],  
+            default: [],
+        }
     },
 
     onLoad() {
-        this.dice=new Dice(  [0,3,0],
+        this.dice=new Dice(  [0,3,0],   
                                 [5,1,2],
                                 [0,4,0]);
-        //cc.log(this.dice)
+        this.countMove = 16;
+        this.moves.getComponent(cc.Label).string = 'Moves: ' + this.countMove;   
+        this.newMap = this.map.getComponent('MapController').createMapWithCsv(0);
+        this.posStart = this.map.getComponent('MapController').renderMap(this.newMap);  
+    },
+
+    setupDice(pos) {
+        this.tilesMap = this.map.getComponent("MapController").tiles;
+        this.node.position = this.tilesMap[pos[0]][pos[1]].position;
+        this.currentDicePos = {
+            row: pos[0],
+            col: pos[1]
+        };
+        cc.log('cr', this.node.position)
+    },
+
+    moveDice(event, direction) {
+        this.tilesMap = this.map.getComponent("MapController").tiles;
+        if (this.isMovingDice) return;
+        this.btnHolder.active = false;
+        let col = this.currentDicePos.col;
+        let row = this.currentDicePos.row;
+
+        switch (Number(direction)) {
+            case DICE_DIRECTION.LEFT:
+                if (col > 0) {
+                    col -= 1;
+                    this.Left();
+                }
+                break;
+            case DICE_DIRECTION.RIGHT:
+                if (col < this.newMap.cols - 1) {
+                    col += 1;
+                    this.Right()
+                }
+                break;
+            case DICE_DIRECTION.UP:
+                if (row > 0) {
+                    row -= 1;
+                    this.Up()
+                }
+                break;
+            case DICE_DIRECTION.DOWN:
+                if (row < this.newMap.rows - 1) {
+                    row += 1;
+                    this.Down()
+                }
+                break;
+        }
+
+        this.currentDicePos = {
+            row,
+            col
+        };
+
+        this.isMovingDice = true;
+        if (-1 < row < this.newMap.rows && -1 < col < this.newMap.cols) {
+            const targetPosition = this.tilesMap[row][col].position;
+            if (targetPosition) {
+                this.node.runAction(cc.sequence(
+                    cc.moveTo(0.3, targetPosition),
+                    cc.callFunc(() => {
+                        this.countMove--;
+                        this.moves.getComponent(cc.Label).string = 'Moves: ' + this.countMove;
+                        if (row === this.newMap.destination[0] && col === this.newMap.destination[1] && this.getDiceFace() === 6) {
+                            alert('You Win')
+                            this.node.active = false;
+                        }
+                        else if (row === this.newMap.destination[0] && col === this.newMap.destination[1] && this.getDiceFace() != 6 || this.countMove === 0) {
+                            alert('you lose')
+                            this.node.active = false;
+                            
+                        };
+                        this.isMovingDice = false;
+                        this.showBtnDirection();
+                    })
+                ));
+            }
+            else {
+                cc.error("ô trên không hợp lệ.");
+            }
+        }
+    },
+
+    showBtnDirection() {
+        this.btnHolder.active = true;
+        this.btnHolder.emit("UPDATE_BTN_CONTROLLER", this.currentDicePos, this.newMap.cols, this.newMap.rows, this.newMap);
+        this.onDiceFaces();
+    },
+
+    onDiceFaces() {
+        this.diceFace = this.getDiceFaces().map((str, index) => {
+            this.btnHolder.emit("UPDATE_DICE_NUMBER", str, index + 1);
+        });
     },
 
     changeAtlas(index){
@@ -49,6 +157,8 @@ cc.Class({
     getDiceFace(){
         return this.dice.diceFace;
     }
+
+    
     
 
 });
--- assets/Scripts/gameController.js
+++ assets/Scripts/gameController.js
@@ -1,135 +1,35 @@
 var Map = require('Map');
-const DICE_DIRECTION = {
-    LEFT: 1,
-    RIGHT: 2,
-    UP: 3,
-    DOWN: 4
-};
+
 cc.Class({
     extends: cc.Component,
 
     properties: {
         map: cc.Node,
         diceController: cc.Node,
-        btnHolder: cc.Node,
-        moves: cc.Label,
-
-        diceFace: {
-            type: [cc.Label],
-            default: [],
-        }
+        
     },
 
     onLoad() {
-        this.createMap();
-        this.countMove = 16;
-        this.diceController.zIndex = 999
-        this.tilesMap = this.map.getComponent("MapController").tiles;
-        this.setupDice();
-        // this.posStart = null;
+        this.startGame(0);
+       
+    },
+
+    startGame(level){
+        
+        this.createMap(level);
+    },
+    createMap(index) {
         this.dice = this.diceController.getComponent("DiceController")
-        this.moves.getComponent(cc.Label).string = 'Moves: ' + this.countMove;
-        cc.log(this.dice._indexDice)
-    },
-
-    start() {
-
-    },
-
-    createMap() {
-        this.newMap = this.map.getComponent('MapController').createMapWithCsv(0);
+        this.newMap = this.map.getComponent('MapController').createMapWithCsv(index);
+        this.map.getComponent('MapController').renderMap(this.newMap)
         this.posStart = this.map.getComponent('MapController').renderMap(this.newMap);
+        this.diceController.zIndex = 999
+        this.dice.setupDice(this.posStart)
+        cc.log(this.posStart)
     },
 
-    setupDice() {
-        this.diceController.position = this.tilesMap[this.posStart[0]][this.posStart[1]].position;
-        this.currentDicePos = {
-            row: this.posStart[0],
-            col: this.posStart[1]
-        };
-    },
+    
 
-    moveDice(event, direction) {
-        if (this.isMovingDice) return;
-        this.btnHolder.active = false;
-        let col = this.currentDicePos.col;
-        let row = this.currentDicePos.row;
-
-        switch (Number(direction)) {
-            case DICE_DIRECTION.LEFT:
-                if (col > 0) {
-                    col -= 1;
-                    this.dice.Left();
-                }
-                break;
-            case DICE_DIRECTION.RIGHT:
-                if (col < this.newMap.cols - 1) {
-                    col += 1;
-                    this.dice.Right()
-                }
-                break;
-            case DICE_DIRECTION.UP:
-                if (row > 0) {
-                    row -= 1;
-                    this.dice.Up()
-                }
-                break;
-            case DICE_DIRECTION.DOWN:
-                if (row < this.newMap.rows - 1) {
-                    row += 1;
-                    this.dice.Down()
-                }
-                break;
-        }
-
-        this.currentDicePos = {
-            row,
-            col
-        };
-
-        this.isMovingDice = true;
-        if (-1 < row < this.newMap.rows && -1 < col < this.newMap.cols) {
-            const targetPosition = this.tilesMap[row][col].position;
-            if (targetPosition) {
-                this.diceController.runAction(cc.sequence(
-                    cc.moveTo(0.3, targetPosition),
-                    cc.callFunc(() => {
-                        this.countMove--;
-                        this.moves.getComponent(cc.Label).string = 'Moves: ' + this.countMove;
-                        if (row === this.newMap.destination[0] && col === this.newMap.destination[1] && this.dice.getDiceFace() === 6) {
-                            alert('You Win')
-                            this.diceController.active = false;
-                        }
-                        else if (row === this.newMap.destination[0] && col === this.newMap.destination[1] && this.dice.getDiceFace() != 6 || this.countMove === 0) {
-                            alert('you lose')
-                            this.diceController.active = false;
-                        };
-                        this.isMovingDice = false;
-                        this.showBtnDirection();
-                    })
-                ));
-            }
-            else {
-                cc.error("ô trên không hợp lệ.");
-            }
-        }
-    },
-
-    showBtnDirection() {
-        this.btnHolder.active = true;
-        this.btnHolder.emit("UPDATE_BTN_CONTROLLER", this.currentDicePos, this.newMap.cols, this.newMap.rows, this.newMap);
-        this.onDiceFaces();
-    },
-
-    onDiceFaces() {
-        this.diceFace = this.dice.getDiceFaces().map((str, index) => {
-            this.btnHolder.emit("UPDATE_DICE_NUMBER", str, index + 1);
-        });
-    },
-
-    undoDice() {
-
-    },
     replayGame(event) {
         return this.gameStartState();
     },
